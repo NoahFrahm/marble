5de1db231ddf1465db769585e609c6fb42d7fb70
NoahFrahm <nofrahm@live.unc.edu>
2022-06-03 19:03:22 -0700

    xcode build
    built xcode project for app

---
+++ marmor/marmor/marmorApp.swift
@@ -0,0 +1,17 @@
+//
+//  marmorApp.swift
+//  marmor
+//
+//  Created by Noah Frahm on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct marmorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
